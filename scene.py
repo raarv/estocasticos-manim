af7d311806c234864c02bbc393d8b97ae9bc609b
Rafael Arredondo <r4f4al@gmail.com>
2022-05-27 19:27:49 -0500

    initial example

---
+++ scene.py
@@ -0,0 +1,36 @@
+from manim import *
+
+class MovingFrameBox(Scene):
+    def construct(self):
+        text=MathTex(
+            "\\frac{d}{dx}f(x)g(x)=","f(x)\\frac{d}{dx}g(x)","+",
+            "g(x)\\frac{d}{dx}f(x)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.wait()
+        self.play(FadeOut(text, framebox2))
+
+        text=MathTex(
+            "\\frac{a}{bx}f(c)g(d)=","f(e)\\frac{f}{gx}g(h)","+",
+            "g(i)\\frac{j}{kx}f(l)"
+        )
+        self.play(Write(text))
+        framebox1 = SurroundingRectangle(text[1], buff = .1)
+        framebox2 = SurroundingRectangle(text[3], buff = .1)
+        self.play(
+            Create(framebox1),
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(framebox1,framebox2),
+        )
+        self.wait()
